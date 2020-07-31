d9087c6d7a722e182e5399063f2fa91e45ca9839
Jasonlee6789 <jeffersonlee@163.com>
2020-07-31 20:24:51 +1200

    I  use webpack to package different js files to use their different functions to make the web run.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import fn from "./fn.js";
 import logo from "./images/logo.png";
 import css from "./css/css.css";
 
-console.log(fn());
+console.log(fn);
 console.log("logo", logo);
 console.log("css", css);
 
